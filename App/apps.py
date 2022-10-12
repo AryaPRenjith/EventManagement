f9620298c2be4c4822648acbd1fddfaa0caa4a44
Arya P Raj <aryaprenjith@gmail.com>
2022-10-12 11:11:02 +0530

    3rd commit

--- App/apps.py
+++ App/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-# Test code
+# Test code 1
+# test code 2
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'App'
